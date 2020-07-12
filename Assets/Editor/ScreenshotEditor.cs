ac93be2dc6d4f6d700036b0e3c2961b9077b89e1
is-up <isup89@gmail.com>
2020-07-12 21:55:29 +0300

    Added turrets (prefabs + icons + configs)

---
+++ Assets/Editor/ScreenshotEditor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+[CustomEditor(typeof(Screenshot))]
+public class ScreenshotEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Take Screenshot"))
+        {
+            var filepath = ((Screenshot)serializedObject.targetObject).TakeScreenshot();
+
+            if (File.Exists(filepath))
+            {
+                var fileBytes = File.ReadAllBytes(filepath);
+            }
+        }
+    }
+}
---
+++ Assets/Src/Configs/TurretConfigProvider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Configs/TurretConfigProvider", fileName = "TurretConfigProvider")]
+public class TurretConfigProvider : ScriptableObject
+{
+    public TurretConfig[] Configs;
+
+    public TurretConfig GetConfig(TurretType type, int levelIndex)
+    {
+        return Configs.FirstOrDefault(c => c.TurretType == type && c.TurretLevelIndex == levelIndex);
+    }
+}
+
+[Serializable]
+public class TurretConfig
+{
+    public TurretType TurretType;
+    public int TurretLevelIndex;
+    public GameObject Prefab;
+    public Sprite IconSprite;
+}
+
+public enum TurretType
+{
+    Gun,
+    Laser,
+    Rocket,
+    SlowField,
+}
