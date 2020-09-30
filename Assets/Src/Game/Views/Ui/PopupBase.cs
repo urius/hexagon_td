da9d16e001a7a4f0a6964fc36b25e2818699dbc5
is-up <isup89@gmail.com>
2020-09-30 23:43:57 +0300

    Win popup pt 1

---
+++ Assets/Src/Game/Views/Ui/PopupBase.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class PopupBase : MonoBehaviour
+{
+    [SerializeField] private Animation _animation;
+
+    private TaskCompletionSource<bool> _showTsc = new TaskCompletionSource<bool>();
+
+    protected Task ShowTask => _showTsc.Task;
+
+    public void EventOnShowAnimationFinished()
+    {
+        _showTsc.SetResult(true);
+    }
+
+    public void EventOnHideAnimationFinished()
+    {
+        Destroy(gameObject);
+    }
+
+    public void Hide()
+    {
+        _animation.Play("Popup hide");
+    }
+}
---
+++ Assets/Src/Game/Views/Ui/WinPopup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinPopup : PopupBase
+{
+    [SerializeField] private Button _nextLevelBtn;
+    [SerializeField] private Button _mainMenuBtn;
+    [SerializeField] private Text _titleTxt;
+    [SerializeField] private Text _infoTxt;
+    [SerializeField] private Image _star1Image;
+    [SerializeField] private Image _star2Image;
+    [SerializeField] private Image _star3Image;
+    [SerializeField] private Color _filledStarColor;
+
+    private void Awake()
+    {
+        _mainMenuBtn.onClick.AddListener(OnMainMenuBtnClick);
+        _nextLevelBtn.onClick.AddListener(OnNextLevelBtnClick);
+    }
+
+    private void OnMainMenuBtnClick()
+    {
+        Hide();
+    }
+
+    private void OnNextLevelBtnClick()
+    {
+        Hide();
+    }
+}
