4042eb4213328b3ba89f4131db5fe34d5baaca82
Юрий <yuris@MacBook-Pro-Urij.local>
2021-01-18 22:56:43 +0300

    Scene top menu buttons for quick transitions

---
+++ Assets/Editor/ScenesMenuFunctions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+public class ScenesMenuFunctions : MonoBehaviour
+{
+    [MenuItem("Scenes/Bootstrapper")]
+    private static void ShowBootstrapScene()
+    {
+        OpenScene("BootstrapScene");
+    }
+
+    [MenuItem("Scenes/MainMenuScene")]
+    private static void ShowMainMenuScene()
+    {
+        OpenScene("MainMenuScene");
+    }
+
+    [MenuItem("Scenes/GameScene")]
+    private static void ShowGameScene()
+    {
+        OpenScene("GameScene");
+    }
+
+    [MenuItem("Scenes/TransitionScene")]
+    private static void ShowTransitionScene()
+    {
+        OpenScene("TransitionScene");
+    }
+    
+    private static void OpenScene(string sceneName)
+    {
+        EditorSceneManager.OpenScene($"Assets/Scenes/{sceneName}.unity");
+    }
+}
