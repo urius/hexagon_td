368e29f1a2f7c096dbd3d17be908e45dc7cbcf0c
Юрий <yuris@MacBook-Pro-Urij.local>
2021-03-18 23:31:55 +0300

    update levels balance + Strat wave button apper logic changed

--- Assets/Src/Game/Views/Ui/StartWaveButtonView.cs
+++ Assets/Src/Game/Views/Ui/StartWaveButtonView.cs
@@ -1,16 +1,33 @@
 ﻿using UnityEngine;
+using DigitalRuby.Tween;
+using UnityEngine.UI;
 
 public class StartWaveButtonView : ButtonView
 {
     [SerializeField] private Animation _animation;
+    [SerializeField] private Button _button;
+    [SerializeField] private CanvasGroup _canvasGroup;
 
     public void SetActiveAnimated(bool isActive)
     {
-        _animation.Play(isActive ? "StartButton_show" : "StartButton_hide");
+        TweenFactory.RemoveTweenKey(_button, TweenStopBehavior.Complete);
+
+        if (isActive) { gameObject.SetActive(true); }
+
+        var alphaFrom = isActive ? 0f : 1f;
+        var alphaTo = 1 - alphaFrom;
+        _button.interactable = isActive;
+        TweenFactory.Tween(_button, alphaFrom, alphaTo, 0.5f, TweenScaleFunctions.Linear,
+            t => _canvasGroup.alpha = t.CurrentValue,
+            t => gameObject.SetActive(isActive));
+
+        _animation.Play("StartButton_idle");
     }
 
-    public void EventOnShowAnimationFinished()
+    protected override void OnDestroy()
     {
-        _animation.Play("StartButton_idle");
+        TweenFactory.RemoveTweenKey(_button, TweenStopBehavior.DoNotModify);
+
+        base.OnDestroy();
     }
 }
